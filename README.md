d4cb7dd5808df1ba3efde4db76f1519bcd6d8657
rhosys-service-account <44204620+rhosys-service-account@users.noreply.github.com>
2020-05-14 20:40:08 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# authress-sdk
+Authress SDK for Python.
+
+[![NuGet version](https://badge.fury.io/py/authress-sdk.svg)](https://badge.fury.io/py/authress-sdk) [![Build Status](https://travis-ci.com/authress/authress-sdk.py.svg?branch=master)](https://travis-ci.com/authress/authress-sdk.py)
+
---
+++ setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup, find_packages
+
+TRAVIS_BUILD_VERSION = os.environ.get('TRAVIS_BUILD_NUMBER') or "0"
+VERSION = f"0.0.{TRAVIS_BUILD_VERSION}"
+REQUIRES = ["urllib3 >= 1.15", "six >= 1.10", "certifi", "python-dateutil"]
+
+# To install the library, run the following
+#
+# python setup.py install
+
+setup(
+  name = 'authress-sdk',
+  version = VERSION,
+  description = 'Authress SDK for authorization as a service and interact with the Authress API.',
+  author = 'Rhosys Developers',
+  author_email = 'developers@authress.io',
+  url = 'https://github.com/Authress/authress-sdk.py.git',
+  include_package_data = True,
+  install_requires=REQUIRES,
+  packages=find_packages(),
+  download_url = f"https://github.com/Authress/authress-sdk.py/tarball/{VERSION}",
+  keywords = ['Authentication', 'Authorization', 'Authorization as a service', 'Security', 'Authress'],
+  classifiers = [],
+)
