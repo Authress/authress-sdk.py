651098c2b53dd3e7c2b9971b92ca6af2d001976e
Warren Parad <wparad@gmail.com>
2020-06-28 15:06:24 +0200

    Use version file.

--- authress_sdk/api_client.py
+++ authress_sdk/api_client.py
@@ -41,7 +41,11 @@ class ApiClient(object):
         self.rest_client = rest.RESTClientObject()
         self.default_headers = {}
         # Set default User-Agent.
-        self.default_headers['User-Agent'] = f'Python AuthressSDK version: 1.0'
+
+        this_directory = os.path.abspath(os.path.dirname(__file__))
+        with open(os.path.join(this_directory, '..', 'VERSION')) as version_file:
+          VERSION = version_file.read().strip()
+        self.default_headers['User-Agent'] = f'Python AuthressSDK version: {VERSION}'
 
     def __del__(self):
         self.pool.close()
--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 import os
 from setuptools import setup, find_packages
 
-TRAVIS_BUILD_VERSION = os.environ['TRAVIS_BUILD_NUMBER'] or "0"
-VERSION = f"1.0.{TRAVIS_BUILD_VERSION}"
 REQUIRES = ["urllib3 >= 1.15", "six >= 1.10", "certifi", "python-dateutil", "PyJWT >= 1.7.1", "cryptography >= 2.9.2"]
 
 # To install the library, run the following
@@ -13,6 +11,9 @@ from os import path
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
   long_description = f.read()
+
+with open(os.path.join(this_directory, 'VERSION')) as version_file:
+  VERSION = version_file.read().strip()
 
 print("Building version", VERSION)
 
@@ -28,7 +29,8 @@ setup(
   packages=find_packages(),
   keywords = ['Authentication', 'Authorization', 'Authorization as a service', 'Security', 'Authress'],
   classifiers = [],
-  license = "Apache-2.0",
+  license = 'Apache-2.0',
+  data_files=[('', ['VERSION'])],
   long_description=long_description,
   long_description_content_type='text/markdown'
 )
