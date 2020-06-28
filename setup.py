05891ebe7490dcc31fab53f42c3386ffc6cf4563
Warren Parad <wparad@gmail.com>
2020-06-28 14:42:44 +0200

    Add print version to setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,8 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
   long_description = f.read()
 
+print("Building version", VERSION)
+
 setup(
   name = 'authress-sdk',
   version = VERSION,
