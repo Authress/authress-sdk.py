2f4dafe7db122f66eedf1510904ce2e54d469aeb
Warren Parad <5056218+wparad@users.noreply.github.com>
2026-01-21 20:53:24 +0700

    Update urllib3 version constraint in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup, find_packages
 
 REQUIRES = [
-  "urllib3 >= 1.25.3, <= 3",
+  "urllib3 >= 1.25.3, < 3",
   "python-dateutil",
   "pydantic >= 1.10.5, < 3",
   "PyJWT >= 2.4.0",
